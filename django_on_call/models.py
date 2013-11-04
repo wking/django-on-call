c008cee41f06ec020c1d180fee4cd408d50fda9e
W. Trevor King <wking@tremily.us>
2013-11-04 15:53:21 -0800

    django_on_call.models: Add an OnCall class
    I'm having second thoughts about the exec() approach, but parsing
    recurrence rules seems too complicated for now.  I may add something
    inspired by RFC 5545 later [1].
    
    [1]: http://tools.ietf.org/html/rfc5545#section-3.3.10

--- django_on_call/models.py
+++ django_on_call/models.py
@@ -1,3 +1,14 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class OnCall (models.Model):
+    rule = models.TextField(
+        verbose_name='on-call rule',
+        help_text='Python statement for determining the on-call admin')
+
+    def get_on_call(self, now=None):
+        if now is None:
+            now = datetime.datetime.now()
+        exec(self.rule)
