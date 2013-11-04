b1b4d3fc1702f3bd6b356b53637586ea9f7c0750
W. Trevor King <wking@tremily.us>
2013-11-04 14:33:14 -0800

    example/example/settings.py: Use a configurable sqlite database backend

--- example/example/settings.py
+++ example/example/settings.py
@@ -1,18 +1,25 @@
 # Django settings for example project.
 
+import os
+
+
+DEFAULT_DATA_DIRECTORY = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), 'data'))
+DATA_DIRECTORY = os.environ.get('DJANGO_ON_CALL_DATA', DEFAULT_DATA_DIRECTORY)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('On Call Guy', 'on-call@example.invalid'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DATA_DIRECTORY, 'sqlite3.db'),
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -49,18 +56,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(DATA_DIRECTORY, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(DATA_DIRECTORY, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
