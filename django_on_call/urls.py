602cbf97ac3fa0c42900681896a6444951afdb4a
W. Trevor King <wking@tremily.us>
2013-11-04 19:30:57 -0800

    django_on_call.urls: Add a ListView URL and associated template
    Using the default template name for a ListView of OnCall instances
    [1].  Also document the new view in the README.
    
    [1]: https://docs.djangoproject.com/en/dev/ref/class-based-views/generic-display/#listview

--- django_on_call/urls.py
+++ django_on_call/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from .models import OnCall
 
 
 urlpatterns = patterns('',
+    url(r'^$',
+        ListView.as_view(model=OnCall),
+        name='on-calls'),
     url(r'^(?P<slug>[-\w\d]+)/$',
         DetailView.as_view(model=OnCall),
         name='on-call'),
