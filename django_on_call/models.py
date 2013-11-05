4aaebe2e1394b200d4e4a9724720fb9db9f4dc1c
W. Trevor King <wking@tremily.us>
2013-11-04 16:13:01 -0800

    django_on_call.models: Return on_call admin from OnCall.get_on_call
    Otherwise there's not much point in the exec() call.

--- django_on_call/models.py
+++ django_on_call/models.py
@@ -9,9 +9,13 @@ class OnCall (models.Model):
         help_text='Identify this among other possible on-call positions')
     rule = models.TextField(
         verbose_name='on-call rule',
-        help_text='Python statement for determining the on-call admin')
+        help_text=(
+            'Python statement for determining the on-call admin.  '
+            "Store the selected admin as 'on_call'."))
 
     def get_on_call(self, now=None):
         if now is None:
             now = datetime.datetime.now()
         exec(self.rule)
+        if 'on_call' in locals():
+            return on_call
